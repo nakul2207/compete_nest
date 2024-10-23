aee07586fa6405ac84298cdc68cfc9e07ce0af72
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2024-10-23 18:20:45 +0530

    setup backend

---
+++ backend/src/app.ts
@@ -0,0 +1,38 @@
+import {config} from "dotenv"
+config()
+
+import express, {Request, Response} from "express"
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
+
+const client = new S3Client({
+    region: process.env.AWS_REGION as string,
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY as string
+    }
+});
+
+async function getObjectURL(key: string){
+    const command = new GetObjectCommand({
+        Bucket: 'compete-nest',
+        Key: key,
+    });
+
+    const url = getSignedUrl(client, command);
+    return url;
+}
+
+app.get("/", async (req:Request, res: Response) =>{
+    const url = await getObjectURL("Problems/sum/input/input_0.txt");
+    console.log(url);
+
+    res.send(url);
+})
+
+export default app;
