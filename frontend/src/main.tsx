efcb4f0070fd1d576fbc7992c1a22c71490f32cc
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2024-12-23 12:37:19 +0530

    normal fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { ChakraProvider } from '@chakra-ui/react'
 // import theme from './theme.ts'
 import { ThemeProvider } from './components/ThemeProvider'
 import {Provider} from "react-redux";
@@ -11,11 +10,9 @@ import {store} from "./redux/store.tsx"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider store={store}>
-        {/*<ChakraProvider theme={theme}>*/}
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        {/*</ChakraProvider>*/}
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
       </Provider>
   </StrictMode>
 )
